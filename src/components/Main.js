8972ee7eaeffd1f7551c9ab43f6a94aa9854df95
geekhac <956854217@qq.com>
2016-10-17 18:06:11 +0800

    init project structor

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,11 @@ require('styles/App.scss');
 import React from 'react';
 
 //获取图片数据
-var imageDatas=require('../data/imageDatas.json');
+let imageDatas = require('../data/imageDatas.json');
 
 //利用自执行函数，将图片的信息转换为图片的url信息
 imageDatas=(function genImageURL(imageDatasArr) {
-  for(let i=0,j=imageDataArr.length;i<j;i++){
+  for(let i=0,j=imageDatasArr.length;i<j;i++){
     let singleImageData=imageDatasArr[i];
 
     singleImageData.imageURL=require('../images/'+singleImageData.fileName);
