29ea82b62d3e281ffb5744ef6dd7b0d31d9dba58
geekhac <956854217@qq.com>
2016-10-18 10:49:22 +0800

    add figure

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ import React from 'react';
 let imageDatas = require('../data/imageDatas.json');
 
 //利用自执行函数，将图片的信息转换为图片的url信息
-imageDatas=(function genImageURL(imageDatasArr) {
+imageDatas=((imageDatasArr) =>{
   for(let i=0,j=imageDatasArr.length;i<j;i++){
     let singleImageData=imageDatasArr[i];
 
@@ -20,13 +20,33 @@ imageDatas=(function genImageURL(imageDatasArr) {
 })(imageDatas);
 
 
+class ImgFigure extends React.Component {
+  render () {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
 class AppComponent extends React.Component {
   render() {
+
+    let controllerUnits=[],imgFigures=[];
+
+    imageDatas.forEach(value =>imgFigures.push(<ImgFigure data={value}/>))
+
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
